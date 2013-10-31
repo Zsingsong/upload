58f02ceaddb876d77e6f73b7b153ddaadc612e1b
singsong <491273636@qq.con>
2013-10-31 21:25:54 +0800

    version1

---
+++ painter/painter/BIDAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  BIDAppDelegate.h
+//  painter
+//
+//  Created by zsingsong on 13-10-29.
+//  Copyright (c) 2013年 singsong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BIDViewController;
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) BIDViewController *viewController;
+
+@end
